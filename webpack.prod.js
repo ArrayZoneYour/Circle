aa13b28ff38c913c8fea22a5abd4ade26329b279
Yidong Li <liyidong@momenta.ai>
2018-09-20 14:28:20 +0800

    :sparkles: (Compress) Compress in the prod; Update the IconFont SourceUrl

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const common = require("./webpack.common.js");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new CompressionPlugin()
+    ]
 });
